936651253df422a6842dfd0b7b4ce3ca2db1365e
영준 윤 <dnfl1563@naver.com>
2024-05-02 22:45:11 +0900

    동영상 재생/일시정지, 스킵 기능 추가

--- Annotation_Tool_Video/Annotation_Tool_Video.cpp
+++ Annotation_Tool_Video/Annotation_Tool_Video.cpp
@@ -10,9 +10,11 @@ Annotation_Tool_Video::Annotation_Tool_Video(QWidget *parent)
     init_ui();
 
     connect(ui.btn_load, SIGNAL(clicked()), this, SLOT(load_video()));
-    //connect(this, &Annotation_Tool_Video::spacePressed, this, &Annotation_Tool_Video::togglePlayback);
+    connect(ui.btn_play_status, SIGNAL(clicked()), this, SLOT(change_status()));
+    connect(ui.btn_skip, SIGNAL(clicked()), this, SLOT(click_skip()));
+    connect(ui.btn_skip_before, SIGNAL(clicked()), this, SLOT(click_skip_before()));
 
-    //installEventFilter(this);
+
 }
 
 Annotation_Tool_Video::~Annotation_Tool_Video()
@@ -21,6 +23,11 @@ Annotation_Tool_Video::~Annotation_Tool_Video()
 void Annotation_Tool_Video::init_ui() {
     QVBoxLayout* layout = new QVBoxLayout(ui.groupBox_video);
     layout->addWidget(videoWidget);
+    ui.btn_play_status->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui.btn_next->setIcon(style()->standardIcon(QStyle::SP_MediaSkipForward));
+    ui.btn_prev->setIcon(style()->standardIcon(QStyle::SP_MediaSkipBackward));
+    ui.btn_skip->setIcon(style()->standardIcon(QStyle::SP_MediaSeekForward));
+    ui.btn_skip_before->setIcon(style()->standardIcon(QStyle::SP_MediaSeekBackward));
 }
 
 void Annotation_Tool_Video::load_video() {
@@ -35,36 +42,65 @@ void Annotation_Tool_Video::load_video() {
 
 
 void Annotation_Tool_Video::play_media(const QString& path) {
-    // 기존의 비디오 위젯과 플레이어를 멤버 변수로 이동시킵니다.
-    
-    //QMediaPlayer* player = new QMediaPlayer(this);
-
-    // 레이아웃 매니저를 사용하여 비디오 위젯의 크기를 부모 위젯에 맞춥니다.
-    //QVBoxLayout* layout = new QVBoxLayout(ui.groupBox_video);
-    //layout->addWidget(videoWidget);
-
-    // 비디오를 재생하기 전에 비디오 소스를 설정하고 재생합니다.
-    
     videoWidget->setFocus();
     player->setSource(QUrl::fromLocalFile(path));
     player->setVideoOutput(videoWidget);
     player->play();
+    ui.btn_play_status->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
     isPlaying = true;
+
+    connect(player, &QMediaPlayer::durationChanged, this, &Annotation_Tool_Video::durationChanged);
+    connect(player, &QMediaPlayer::positionChanged, this, &Annotation_Tool_Video::positionChanged);
+
+    ui.slider_duration->setRange(0, player->duration() / 1000);
 }
 
-
-bool Annotation_Tool_Video::eventFilter(QObject* obj, QEvent* event) {
-    if (obj == this && event->type() == QEvent::KeyPress) {
-        QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
-        if (keyEvent->key() == Qt::Key_Space) {
-            if (isPlaying) {
-                player->pause();
-            }
-            else {
-                player->play();
-            }
-            return true; // Event handled
-        }
+void Annotation_Tool_Video::change_status() {
+    if (isPlaying) {
+        player->pause();
+        ui.btn_play_status->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+        isPlaying = false;
     }
-    return QMainWindow::eventFilter(obj, event);
+    else if(!isPlaying){
+        player->play();
+        ui.btn_play_status->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+        isPlaying = true;
+    }
+}
+
+void Annotation_Tool_Video::click_skip() {
+    ui.slider_duration->setValue(ui.slider_duration->value() + 10);
+    player->setPosition(ui.slider_duration->value() * 1000);
+}
+
+void Annotation_Tool_Video::click_skip_before() {
+    ui.slider_duration->setValue(ui.slider_duration->value() - 10);
+    player->setPosition(ui.slider_duration->value() * 1000);
+}
+
+void Annotation_Tool_Video::updateDuration(qint64 duration) {
+    if (duration || mDuration)
+    {
+        QTime CurrentTime((duration / 3600) % 60, (duration / 60) % 60, duration % 60, (duration * 1000) % 1000);
+        QTime TotalTime((mDuration / 3600) % 60, (mDuration / 60) % 60, mDuration % 60, (mDuration * 1000) % 1000);
+        QString Format = "";
+        if (mDuration > 3600) Format = "hh:mm:ss";
+        else Format = "mm:ss";
+
+        ui.lbl_presentTime->setText(CurrentTime.toString(Format));
+        ui.lbl_totalTime->setText(TotalTime.toString(Format));
+    }
+}
+
+void Annotation_Tool_Video::durationChanged(qint64 duration) {
+    mDuration = duration / 1000;
+    ui.slider_duration->setMaximum(mDuration);
+}
+
+void Annotation_Tool_Video::positionChanged(qint64 duration) {
+    if (!ui.slider_duration->isSliderDown())
+    {
+        ui.slider_duration->setValue(duration / 1000);
+    }
+    updateDuration(duration / 1000);
 }
--- Annotation_Tool_Video/Annotation_Tool_Video.h
+++ Annotation_Tool_Video/Annotation_Tool_Video.h
@@ -25,14 +25,20 @@ private:
     Ui::Annotation_Tool_VideoClass ui;
     QMediaPlayer* mediaPlayer;
     bool isPlaying = false;
+    qint64 mDuration;
 
     QStringList file_list;
 
     void play_media(const QString& path);
-    bool eventFilter(QObject* obj, QEvent* event) override;
+    void updateDuration(qint64 duration);
 
 private slots:
     void load_video();
+    void change_status();
+    void click_skip();
+    void click_skip_before();
+    void durationChanged(qint64 duration);
+    void positionChanged(qint64 duration);
 
 protected:
     
